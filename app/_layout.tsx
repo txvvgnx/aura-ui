8be581c0fcabb7f8f1bc7caa85b23dd6d8273467
txvvgnx <txvvgnx@gmail.com>
2025-05-06 22:20:03 +0800

    initial expo, nativewind, custom font setup

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,20 @@
 import '../global.css';
 
+import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 
 export default function Layout() {
+  const [fontsLoaded] = useFonts({
+    'IBMPlexSans-Thin': require('../assets/fonts/IBMPlexSans-Thin.ttf'),
+    'IBMPlexSans-ExtraLight': require('../assets/fonts/IBMPlexSans-ExtraLight.ttf'),
+    'IBMPlexSans-Light': require('../assets/fonts/IBMPlexSans-Light.ttf'),
+    'IBMPlexSans-Regular': require('../assets/fonts/IBMPlexSans-Regular.ttf'),
+    'IBMPlexSans-Medium': require('../assets/fonts/IBMPlexSans-Medium.ttf'),
+    'IBMPlexSans-SemiBold': require('../assets/fonts/IBMPlexSans-SemiBold.ttf'),
+    'IBMPlexSans-Bold': require('../assets/fonts/IBMPlexSans-Bold.ttf'),
+  });
+
+  if (!fontsLoaded) return null;
+
   return <Stack />;
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,19 @@ module.exports = {
 
   presets: [require('nativewind/preset')],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['IBMPlexSans-Regular', 'sans-serif'],
+        ibm: ['IBMPlexSans-Regular', 'sans-serif'],
+        'ibm-thin': ['IBMPlexSans-Thin', 'sans-serif'],
+        'ibm-extralight': ['IBMPlexSans-ExtraLight', 'sans-serif'],
+        'ibm-light': ['IBMPlexSans-Light', 'sans-serif'],
+        'ibm-regular': ['IBMPlexSans-Regular', 'sans-serif'],
+        'ibm-medium': ['IBMPlexSans-Medium', 'sans-serif'],
+        'ibm-semibold': ['IBMPlexSans-SemiBold', 'sans-serif'],
+        'ibm-bold': ['IBMPlexSans-Bold', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 };
